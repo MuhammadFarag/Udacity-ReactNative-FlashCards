2d7b23bf2ad5f0e1488ecf4b1c1ac37807faa960
Muhammd Farag <muhammadfarag@gmail.com>
2020-06-03 20:12:34 -0400

    feat(navigation): Create Stacked navigation from home screen to view deck

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {FlatList, SafeAreaView, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import Constants from 'expo-constants';
 import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const decks = [
   {name: "deck 1", cards: 3},
@@ -10,19 +11,33 @@ const decks = [
   {name: "deck 3", cards: 15}
 ]
 
-function Deck({name, cards}) {
+function Deck({name, cards, navigation}) {
   return (
+    <TouchableOpacity
+      onPress={() => {
+        console.log("Pressed ", name)
+        navigation.navigate('Deck');
+      } }
+    >
     <View style={styles.item}>
       <Text style={styles.deckName}>{name}</Text>
       <Text style={styles.deckCards}>{cards} Cards</Text>
     </View>
+    </TouchableOpacity>
   );
 }
 
-function Home() {
+function ViewDeck() {
+  return <View>
+    <Text>View Deck</Text>
+  </View>
+}
+
+const Stack = createStackNavigator();
+function Decks({navigation}) {
   return <FlatList
     data={decks}
-    renderItem={({item: deck}) => <Deck name={deck.name} cards={deck.cards}/>}
+    renderItem={({item: deck}) => <Deck name={deck.name} cards={deck.cards} navigation={navigation}/>}
     keyExtractor={deck => deck.name}
   />
 }
@@ -36,19 +51,26 @@ function AddCard() {
 const Tabs = createMaterialTopTabNavigator();
 
 
+function Home() {
+  return <Tabs.Navigator
+    tabBarOptions={{
+      labelStyle: {fontSize: 12},
+      style: {backgroundColor: "powderblue"},
+    }}
+  >
+    <Tabs.Screen name="Home" component={Decks}/>
+    <Tabs.Screen name="Add Card" component={AddCard}/>
+  </Tabs.Navigator>;
+}
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
-        <Tabs.Navigator
-          tabBarOptions={{
-            labelStyle: { fontSize: 12 },
-            style: { backgroundColor: 'powderblue' },
-          }}
-        >
-          <Tabs.Screen name="Home" component={Home}/>
-          <Tabs.Screen name="Add Card" component={AddCard}/>
-        </Tabs.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Deck" component={ViewDeck} />
+        </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
   );
