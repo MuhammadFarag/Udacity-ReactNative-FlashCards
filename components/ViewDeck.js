5f425b697a1448e279e5154760b7b24a1ed88221
Muhammd Farag <muhammadfarag@gmail.com>
2020-06-07 17:28:30 -0400

    feat(deck): Display deck name and number of cards on deck view

--- components/ViewDeck.js
+++ components/ViewDeck.js
@@ -1,9 +1,13 @@
 import {Button, Text, View} from "react-native";
 import * as React from "react";
+import {useSelector} from "react-redux";
 
 export function ViewDeck({navigation, route: {params: {deckId}}}) {
+  const {name, cards} = useSelector(state => state.decks[deckId])
+
   return <View>
-    <Text>View Deck</Text>
+    <Text>{name}</Text>
+    <Text>{cards.length} Cards</Text>
     <Button
       title="Add Card"
       onPress={() => {
