c5022574d7cb0f33d514f8227455df3ea16e4c2f
Muhammd Farag <muhammadfarag@gmail.com>
2020-06-09 20:19:18 -0400

    feat(UI): Add animation to ViewDeck screen

--- components/ViewDeck.js
+++ components/ViewDeck.js
@@ -1,11 +1,16 @@
-import {Button, Text, View} from "react-native";
+import {Animated, Button, Text} from "react-native";
 import * as React from "react";
 import {useSelector} from "react-redux";
 
 export function ViewDeck({navigation, route: {params: {deckId}}}) {
   const {name, cards} = useSelector(state => state.decks[deckId])
+  const opacity = React.useRef(new Animated.Value(0)).current
 
-  return <View>
+  React.useEffect(() => {
+    Animated.timing(opacity, {toValue: 1, duration: 1000}).start()
+  })
+
+  return <Animated.View style={[{opacity}]}>
     <Text>{name}</Text>
     <Text>{cards.length} Cards</Text>
     <Button
@@ -24,5 +29,5 @@ export function ViewDeck({navigation, route: {params: {deckId}}}) {
         })
       }}
     />
-  </View>
+  </Animated.View>
 }
